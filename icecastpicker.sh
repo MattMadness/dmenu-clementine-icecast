fd27b0547f84e67d2d67d2f5db49327c3cbc903b
Matthew Evan <mattmadness@420blaze.it>
2021-05-22 21:45:40 -0400

    Updated README

--- icecastpicker.sh
+++ icecastpicker.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# Copyright (C) 2021 Matthew "Madness" Evan
+
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 clementine -pl $(sqlite3 -list ~/.config/Clementine/clementine.db "SELECT url FROM icecast_stations WHERE name = $(sqlite3 -list ~/.config/Clementine/clementine.db 'SELECT name FROM icecast_stations;' | dmenu -l 3 -p "Clementine Icecast:" | sed 's/$/"/' | sed 's/^/"/' );")
